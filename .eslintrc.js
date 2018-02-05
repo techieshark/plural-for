979a118b4a93b5ff2b52b3546e5a7fe253beb0cd
Peter W <techieshark@gmail.com>
2018-02-05 00:29:58 -0800

    :tada: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "commonjs": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:flowtype/recommended"
+  ],
+  "plugins": [
+    "flowtype"
+  ],
+  "rules": {
+    "flowtype/define-flow-type": 1
+  },
+}
